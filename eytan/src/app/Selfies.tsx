6fae47d3c59f361faf3ade612fdcd503386cc13d
Avery Fisher <averyfisher@Averys-MacBook-Pro.local>
2025-01-28 19:12:14 -0800

    Clean linting errors

--- eytan/src/app/Selfies.tsx
+++ eytan/src/app/Selfies.tsx
@@ -1,8 +1,5 @@
 import styles from "./page.module.css";
-import headshot from "./headshot.png"
-import Image from 'next/image';
-import back from "./back.png"
-import React, { useState } from 'react';
+import React from 'react';
 
 interface SelfiesProps {
     onCloseSelf: () => void;
