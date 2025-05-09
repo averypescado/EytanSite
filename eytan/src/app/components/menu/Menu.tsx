90766b5256bfe58ab2b4da480a9873a461bae1ad
Avery Fisher <averydwfisher@gmail.com>
2025-05-08 21:54:22 -0700

    lowercase

--- eytan/src/app/components/menu/Menu.tsx
+++ eytan/src/app/components/menu/Menu.tsx
@@ -9,44 +9,44 @@ interface MenuProps {
 export default function Menu({ className}: MenuProps ) {
     return (
         <ul className={`${styles.list} ${className || ''}`}> 
-            <Link href="/work/HoopDreams2">
+            <Link href="/work/hoopdreams2">
                 <li className={styles.project}>
                     Hoop Dreams 2
                 </li>
             </Link>
-            <Link href="/work/HoopDreams3D">
+            <Link href="/work/hoodDreams3D">
                 <li className={styles.project}>Hoop Dreams 3D</li>
             </Link>
 
-            <Link href="/work/TheToyotaProblem">
+            <Link href="/work/thetoyotatroblem">
                 <li className={styles.project}>The Toyota Problem</li>
             </Link>
             
-            <Link href="/work/RWordIsBack">
+            <Link href="/work/rwordisback">
                 <li className={styles.project}>The R word Is Back</li>
             </Link>
 
-            <Link href="/work/NamePoem">
+            <Link href="/work/namepoem">
                 <li className={styles.project}>Name Poem</li>
             </Link>
             
-            <Link href="/work/Standup">
+            <Link href="/work/standup">
                 <li className={styles.project}>Standup</li>
             </Link>
-            <Link href="/work/Periodical">
+            <Link href="/work/periodical">
                 <li className={styles.project}>Periodical</li>
             </Link>
-            <Link href="/work/ArgentineSoccerAnnouncer">
+            <Link href="/work/argentinesoccerannouncer">
                 <li className={styles.project}>Argentine Soccer Announcer Looking at a Tree</li>
             </Link>
-            <Link href="/work/Bronsons">
+            <Link href="/work/bronsons">
                 <li className={styles.project}>Bronsons Honey</li>
             </Link>
 
-            <Link href="/work/RememberKramer">
+            <Link href="/work/rememberkramer">
                 <li className={styles.project}>Remember Kramer? From Seinfeld?</li>
             </Link>
-            <Link href="/work/BettermanScreenplay">
+            <Link href="/work/bettermanscreenplay">
                 <li className={styles.project}>Better Man Screenplay</li>
             </Link>
         </ul>
